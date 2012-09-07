8cf187d81acc7a0c5f7e357a12367dcc7818288c
sb <hiphophe@gmail.com>
2012-09-06 22:02:59 -0400

    rrdepot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rrdepot::Application.config.secret_token = 'f66368ace97e23514997035aaebde9aec785b52fc4ccdb1189c680b04db7f1585e8687d9efebc434bb1088cfb10503c0049fc9c055278b453ae72a0c1f662ee0'
