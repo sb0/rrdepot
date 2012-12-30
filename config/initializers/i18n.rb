1f6ca3798ae7509b13d8f71c75d67e50c8c7fc86
sb0 <hiphophe@gmail.com>
2012-12-30 01:54:01 -0500

    Iteration J2.001

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,8 +1,8 @@
 I18n.default_locale = :en
 
 LANGUAGES = [
-  ['English',       'en']
-  ['Fran&ccedil;ais'.html_safe,        'fr']
-  ['Espa&ntilde;ol'.html_safe,       'es']
+  ['English',       'en'],
+  ['Fran&ccedil;ais'.html_safe,        'fr'],
+  ['Espa&ntilde;ol'.html_safe,       'es'],
   ['Haitian',       'ht']
 ]
