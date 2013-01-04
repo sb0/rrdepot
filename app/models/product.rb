3e6d6948a80cbc534e3a9e0f08c232f63323b31f
sb0 <hiphophe@gmail.com>
2013-01-04 00:19:39 -0500

    Iteration K1.003

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,8 +3,6 @@ class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
   has_many :line_items
   has_many :orders, :through => :line_items
-  before_destroy :ensure_not_referenced_by_any_line_item
-  validates :title, :description, :image_url, :presence => true
   validates :title, :uniqueness => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :image_url, :format => {
